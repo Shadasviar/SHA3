6156a9d50c21b750e1025dc9c6154f53257255da
Shadasviar <shadasviarus@gmail.com>
2017-12-25 23:39:46 +0300

    Fix state creating.
    Add default word length (64 bits)

--- permutations.py
+++ permutations.py
@@ -1,4 +1,5 @@
 import init_data
+import operator
 
 def _extend_word(word, size):
     res = list(bytearray(word))
@@ -13,7 +14,7 @@ def _split (n, list):
 _j = range(0, init_data.box_size)
 
 # apply f(i,j,k,a) for every element of A with k from 0 to w
-def _map_indexed (f, A, w):
+def _map_indexed (f, A, w = 64):
     _k = range(0,w)
     res = A
     for i in _j:
@@ -22,40 +23,46 @@ def _map_indexed (f, A, w):
                 res[i][j][k] = f(i, j, k, A)
     return res
 
-def _theta (A, w):
+def _theta (A, w = 64):
     def C(i, k):
         return reduce(operator.xor, map(lambda x: A[i][x][k], _j))
     def D(i, k):
-        return C((i-1) % 5, k) ^ C((i+1) % 5, (k-1) % w)
+        return C((i-1) % init_data.box_size, k) ^ C((i+1) % init_data.box_size, (k-1) % w)
 
     return _map_indexed(lambda i,j,k,a: a[i][j][k] ^ D(i,k), A, w)
 
-def _ro (A, w):
+def _ro (A, w = 64):
     _k = range(0,w)
     res = A
     for k in _k:
         res[0][0][k] = A[0][0][k]
-    t = range(0,24)
+    t_range = range(0,24)
     (i, j) = (1,0)
-    for k in _k:
-        res[i][j][k] = A[i][j][(k - (t + 1)*(t + 2)/2) % w]
-        (i,j) = (j, (2*i + 3*j) % 5)
+    for t in t_range:
+	for k in _k:
+	    res[i][j][k] = A[i][j][(k - (t + 1)*(t + 2)/2) % w]
+	    (i,j) = (j, (2*i + 3*j) % init_data.box_size)
     return res
 
-def _pi (A, w):
-    return _map_indexed(lambda i,j,k,a: a[(i + 3*j) % 5][i][k], A, w)
+def _pi (A, w = 64):
+    return _map_indexed(lambda i,j,k,a: a[(i + 3*j) % init_data.box_size][i][k], A, w)
 
-def _xi (A, w):
-    return _map_indexed(lambda i,j,k,a: a[i][j][k] ^ ((a[(i+1) % 5][j][k] ^ 1) * a[(i+2) % 5][j][k]), A, w)
+def _xi (A, w = 64):
+    return _map_indexed(
+	    lambda i,j,k,a: a[i][j][k]
+	    ^ ((a[(i+1) % init_data.box_size][j][k] ^ 1)
+		* a[(i+2) % init_data.box_size][j][k]),
+	    A, w)
 
-def _iota (A, rnd, w):
+def _iota (A, rnd, w = 64):
     _k = range(0,w)
     res = A
     for k in _k:
-        res[0][0][k] = A[0][0][k] ^ rnd
+        res[0][0] = map(operator.xor, A[0][0], rnd)
+    return res
 
-def _str_to_state (S, w):
+def _str_to_state (S, w = 64):
     return _map_indexed(
-            lambda i,j,k,a: (5*i + j) * w + S[k], 
-            [[range(0,w)] * (_j[-1]+1)] * (_j[-1]+1),
+            lambda i,j,k,a: (init_data.box_size*i + j) * w + S[k], 
+	    [[[a for a in range(w)] for c in _j] for d in _j],
             w)

0d49c2b839289cf44f8b59c08f174522f72879cb
Shadasviar <shadasviarus@gmail.com>
2017-12-26 13:33:01 +0300

    Fix iota RC xoring.

--- permutations.py
+++ permutations.py
@@ -4,19 +4,19 @@ import operator
 def _to_bytestr(word):
     return list(bytearray(word))
 
-def _extend_word(word, size = 64):
+def _extend_word(word, size = init_data.word_len):
     ext_len = size-len(word)%size
     if (len(word) % size != 0):
         word += [0x1] + [0x0]*(ext_len-2) + [0x80]*(ext_len-abs(ext_len-2)-1)
     return word
 
-def _split_every (list, n = 64):
+def _split_every (list, n):
     return [list[i:i+n] for i in range(0,len(list), n)]
 
 _j = range(0, init_data.box_size)
 
 # apply f(i,j,k,a) for every element of A with k from 0 to w
-def _map_indexed (f, A, w = 64):
+def _map_indexed (f, A, w = init_data.word_len):
     _k = range(0,w)
     res = A
     for i in _j:
@@ -25,7 +25,7 @@ def _map_indexed (f, A, w = 64):
                 res[i][j][k] = f(i, j, k, A)
     return res
 
-def _theta (A, w = 64):
+def _theta (A, w = init_data.word_len):
     def C(i, k):
         return reduce(operator.xor, map(lambda x: A[i][x][k], _j))
     def D(i, k):
@@ -33,7 +33,7 @@ def _theta (A, w = 64):
 
     return _map_indexed(lambda i,j,k,a: a[i][j][k] ^ D(i,k), A, w)
 
-def _ro (A, w = 64):
+def _ro (A, w = init_data.word_len):
     _k = range(0,w)
     res = A
     for k in _k:
@@ -46,24 +46,29 @@ def _ro (A, w = 64):
 	    (i,j) = (j, (2*i + 3*j) % init_data.box_size)
     return res
 
-def _pi (A, w = 64):
+def _pi (A, w = init_data.word_len):
     return _map_indexed(lambda i,j,k,a: a[(i + 3*j) % init_data.box_size][i][k], A, w)
 
-def _xi (A, w = 64):
+def _xi (A, w = init_data.word_len):
     return _map_indexed(
 	    lambda i,j,k,a: a[i][j][k]
 	    ^ ((a[(i+1) % init_data.box_size][j][k] ^ 1)
 		* a[(i+2) % init_data.box_size][j][k]),
 	    A, w)
 
-def _iota (A, rnd, w = 64):
+def _test_bit (word, n):
+    if (word & (1 << n) != 0):
+	return 1
+    return 0
+
+def _iota (A, rnd, w = init_data.word_len):
     _k = range(0,w)
     res = A
     for k in _k:
-	res[0][0] = map(operator.xor, A[0][0], map(lambda x: rnd & (1 << x), range(0,w)))
+	res[0][0] = map(operator.xor, A[0][0], map(lambda x: _test_bit(rnd, x), range(0,w)))
     return res
 
-def _str_to_state (S, w = 64):
+def _str_to_state (S, w = init_data.word_len):
     return _map_indexed(
             lambda i,j,k,a: (init_data.box_size*i + j) * w + S[k], 
 	    [[[a for a in range(w)] for c in _j] for d in _j],

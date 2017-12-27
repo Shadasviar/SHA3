6fa1da30be3d5d1873f87735e31c52a75a7d41f2
Shadasviar <shadasviarus@gmail.com>
2017-12-27 13:58:06 +0300

    Fix rot visability.

--- permutations.py
+++ permutations.py
@@ -11,8 +11,6 @@ def _pad_word(word, size):
 	word += to_str([chr(0x1)] + [chr(0x0)]*(ext_len-2) + [chr(0x80)]*(ext_len-abs(ext_len-2)-1))
     return word
 
-_j = range(0, init_data.box_size)
-
 def _map_indexed_2d (f, A):
     res = copy.deepcopy(A)
     for x in range(len(A)):
@@ -22,16 +20,16 @@ def _map_indexed_2d (f, A):
 
 def _theta(A, w = init_data.r):
     def C(x):
-	return reduce(operator.xor, map(lambda y: A[x][y], _j))
+	return reduce(operator.xor, map(lambda y: A[x][y], range(init_data.box_size)))
     def D(x):
-	return C((x-1) % init_data.box_size) ^ (rot(C((x+1) % init_data.box_size), 1, w))
+	return C((x-1) % init_data.box_size) ^ (utils.rot(C((x+1) % init_data.box_size), 1, w))
     return _map_indexed_2d(lambda x,y,a: a[x][y] ^ D(x), A)
 
 def _ro_pi(A, w = init_data.r):
     B = copy.deepcopy(A)
-    for x in _j:
-	for y in _j:
-	    B[y][(2*x + 3*y) % init_data.box_size] = rot(A[x][y], init_data.r_offsets[x][y], w)
+    for x in range(len(A)):
+	for y in range(len(A[x])):
+	    B[y][(2*x + 3*y) % init_data.box_size] = utils.rot(A[x][y], init_data.r_offsets[x][y], w)
     return B
 
 def _xi(A, w = init_data.r):
